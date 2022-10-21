73de5c80fe95c8c9d388814e3983204fd0dd88c5
Vale-riad <valentinariad91@gmail.com>
2022-10-21 15:46:58 +0200

    Aggiunge contentuto in html e in js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// 1. chiedo all'utente nome e cognome
+const nomeCognome=document.getElementById("name").value;
+console.log(nomeCognome);
+// 2. chiedo all'utente quanti km vuole percorrere
+const distance=number(document.getElementById("km").value);
+console.log(distance);
