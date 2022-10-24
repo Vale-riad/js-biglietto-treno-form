7409e7081f7f7d552119f04859b9d2c142c40333
Vale-riad <valentinariad91@gmail.com>
2022-10-24 15:17:46 +0200

    Agg esercizio corretto a lezione

--- js/main.js
+++ js/main.js
@@ -1,40 +1,50 @@
 "use strict";
-// 1. chiedo all'utente nome e cognome
-const nomeCognome=document.getElementById("name").value;
-console.log(nomeCognome);
-// 2. chiedo all'utente quanti km vuole percorrere
-const km=Number(document.getElementById("km").value);
-console.log(km);
-// 3. chiedo all'utente la sua fascia d'età
-const age=document.getElementById("age").value;
-console.log(age);
-// 4. calcola la tariffa in base al km
-let price =(0.21 * km);
-console.log(price);
-// 5. applicare uno sconto del 20% se il passeggero è minorenne
+// - Il numero di chilometri da percorrere
+// - Età del passeggero
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+// - il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// - va applicato uno sconto del 20% per i minorenni
+// - va applicato uno sconto del 40% per gli over 65.
+// const btnTicketGenerator = document.getElementById("generator");
+const ticketForm = document.getElementById("ticket-form");
+const btnTicketReset = document.getElementById("reset");
+const ticketContainer =  document.querySelector('.ticket-container');
 
+ticketForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    // 1. leggere i valori del form
+        // 1a. leggo il nome
+        // 1b. leggo i km
+        // 1c. leggo la fascia d'età (age)
+    const name = document.getElementById('name').value;
+    const km = Number(document.getElementById('km').value);
+    const age = document.getElementById('age').value;
+    // 2. Calcolo il prezzo del biglietto e l'offerta
+        // 2a. calcolo il prezzo base km * 0.21 con offerta "Biglietto standard"
+        // 2b. verifico se devo applicare uno sconto age == underage || age == over e aggiorno il testo dell'offerta
+    let price = km * 0.21;
+    let offert = "Biglietto Standard";
 
-const btnGenerator = document.getElementById('btn-generator');
-btnGenerator.addEventListener('click', function() {
-  const nomeCognome = document.getElementById('name');
-  console.log(nomeCognome.value);
-  nomeCognome.value = '';
-  const km = document.getElementById('km');
-  console.log(km.value);
-  km.value = '';
-  let price =(0.21 * km);
-console.log(price);
-// 5. applicare uno sconto del 20% se il passeggero è minorenne
-if (age<18) {
-    price = price-(price * 0.2);
-    console.log(price+("minorenne"));
-    document.getElementById("age").innerHTML=("Minorenne");
-}
-// .6 applicare uno sconto del 40% se il passeggero è un over 65
-else if (age>=65) {
-    price = price-(price * 0.4);
-    console.log(price+(" over-65"));
-    document.getElementById("age").innerHTML=("Over-65");
-}
-console.log("${price.toFixed(2)}€");
+    if( age === 'underage' ) {
+        price = price * 0.8;
+        offert = "Biglietto minorenni";
+    } else if ( age === 'over' ) {
+        price = price * 0.6;
+        offert = "Biglietto over 65";
+    }
+    // 3. mostrare il biglietto a schermo (stampo il risultato)
+    // console.log(`${price.toFixed(2)}€`, offert);
+    document.getElementById('ticket-name').innerHTML = name;
+    document.getElementById('ticket-offert').innerHTML = offert;
+    document.getElementById('ticket-wagon').innerHTML = Math.floor(Math.random() * 10) + 1;
+    document.getElementById('ticket-cp').innerHTML = Math.floor(Math.random() * 10000) + 90000;
+    document.getElementById('ticket-price').innerHTML = `${price.toFixed(2)}€`;
+
+    ticketContainer.classList.add('show');
 });
+
+// Quando clicco su annulla
+btnTicketReset.addEventListener('click', function() {
+    ticketContainer.classList.remove('show');
+});
+  
